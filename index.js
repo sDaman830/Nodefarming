de7d9d7951b18941d29eebc10f6cf7897e48ee2a
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-07-31 19:39:25 +0530

    templ

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-// const textin = fs.readFileSync('./txt/input.txt', 'utf-8')
-// const hello = 'Hello world';
-// console.log(textin);
 
-// Server
-const replateTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME}/g, product.productName);
+// Helper function to replace placeholders in the template
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%FROM%}/g, product.from);
@@ -16,46 +13,39 @@ const replateTemplate = (temp, product) => {
     output = output.replace(/{%DESCRIP%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
 
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`);
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, `not-organic`);
+    }
     return output;
-}
-const data = fs.readFileSync('./dev-data/data.json', 'utf-8', (err, data) => {
+};
 
+// Read data from data.json and template files
+const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
+const tempOverview = fs.readFileSync('/Users/damanpreetsinghghatoura/Desktop/Nodefarming/templates/template-overview.html', 'utf-8');
+const tempCard = fs.readFileSync('/Users/damanpreetsinghghatoura/Desktop/Nodefarming/templates/template-card.html', 'utf-8');
+const tempProduct = fs.readFileSync('/Users/damanpreetsinghghatoura/Desktop/Nodefarming/templates/template-card.html', 'utf-8');
 
-});
-const tempOverview = fs.readFileSync(`/Users/damanpreetsinghghatoura/Desktop/Nodefarming/templates/template-overview.html`, `utf-8`);
-
-const tempcard = fs.readFileSync(`/Users/damanpreetsinghghatoura/Desktop/Nodefarming/templates/template-overview.html`, `utf-8`);
-const tempProduct = fs.readFileSync(`/Users/damanpreetsinghghatoura/Desktop/Nodefarming/templates/template-product.html`, `utf-8`);
 const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    console.log(req.url);
     const pathName = req.url;
-    // res.end('Hello from the server');
+
     if (pathName === '/' || pathName === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
-        const cardsHtml = dataObj.map(el => replateTemplate(tempcard, el)).join('');
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml);
-        console.log(tempOverview);
         res.end(output);
-
     } else if (pathName === '/product') {
         res.end('This is the product page');
-
     } else if (pathName === '/api') {
-
         res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(JSON.stringify(productData));
-
-
-        //res.end('API');
-    }
-    else {
+    } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello world'
         });
-        res.end('<h1>page not found</h1>');//browser expects hrml
+        res.end('<h1>page not found</h1>');
     }
 });
 
